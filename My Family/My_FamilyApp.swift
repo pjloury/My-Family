db9499935396d9e1ac1d7591e9a55996ff7a3c82
PJ <pjloury@gmail.com>
2025-07-26 20:57:48 -0700

    Initial Commit

---
+++ My Family/My_FamilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_FamilyApp.swift
+//  My Family
+//
+//  Created by PJ Loury on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct My_FamilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
